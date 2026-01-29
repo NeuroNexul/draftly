e44d67bc9d9d7773d21dbcfdb0ac08096bde7dd6
Arif Sardar <arifsardar.private@gmail.com>
2026-01-29 23:53:11 +0530

    feat(markly): tsup config update

--- packages/markly/tsup.config.ts
+++ packages/markly/tsup.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ['src/index.ts'],
-  format: ['esm'],
+  entry: ["src/index.ts"],
+  format: ["esm"],
   dts: true,
   splitting: true,
   sourcemap: true,
   clean: true,
   treeshake: true,
-  external: []
+  external: [
+    "@codemirror/state",
+    "@codemirror/view",
+    "@codemirror/language",
+    "@codemirror/commands",
+    "@lezer/markdown",
+    "@lezer/common",
+    "@lezer/highlight",
+  ],
 });
