ca99dc0007ffb5662d9800c46dcc21c33b42484f
Arif Sardar <arifsardar.private@gmail.com>
2026-02-04 22:11:00 +0530

    feat(draftly): Image Plugin... Need to refine more

--- packages/draftly/src/plugins/index.ts
+++ packages/draftly/src/plugins/index.ts
@@ -3,6 +3,7 @@ export { HeadingPlugin } from "./heading-plugin";
 export { InlinePlugin } from "./inline-plugin";
 export { ListPlugin } from "./list-plugin";
 export { HTMLPlugin } from "./html-plugin";
+export { ImagePlugin } from "./image-plugin";
 
 // Plugin collections
 import { DraftlyPlugin } from "../editor/plugin";
@@ -10,13 +11,20 @@ import { HeadingPlugin } from "./heading-plugin";
 import { InlinePlugin } from "./inline-plugin";
 import { ListPlugin } from "./list-plugin";
 import { HTMLPlugin } from "./html-plugin";
+import { ImagePlugin } from "./image-plugin";
 
 /**
  * Default plugins
  *
  * This is the set of essential plugins
  */
-const essentialPlugins: DraftlyPlugin[] = [new HeadingPlugin(), new InlinePlugin(), new ListPlugin(), new HTMLPlugin()];
+const essentialPlugins: DraftlyPlugin[] = [
+  new HeadingPlugin(),
+  new InlinePlugin(),
+  new ListPlugin(),
+  new HTMLPlugin(),
+  new ImagePlugin(),
+];
 
 /**
  * All plugins
